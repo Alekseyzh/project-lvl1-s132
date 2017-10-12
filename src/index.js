a2942e8d5eea4ca73ac35aae72bafc939dc90b6b
Aleksei Zhighitov <allbeehappy@gmail.com>
2017-10-13 06:35:21 +0800

    Fix constant name

--- src/index.js
+++ src/index.js
@@ -2,24 +2,24 @@ import readlineSync from 'readline-sync';
 
 const isEven = num => (num % 2 === 0 ? 'yes' : 'no');
 
-const numAttempt = 3;
+const numberAttempt = 3;
 
 const brainGames = () => {
   console.log('Welcome to the Brain Games!');
   console.log('Answer "yes" if number even otherwise answer "no".\n');
-  const getName = readlineSync.question('May I have your name?: ');
-  console.log(`Hello, ${getName}! \n`);
-  for (let i = 0; i <= numAttempt; i += 1) {
-    if (i === numAttempt) {
-      console.log(`Congratulations, ${getName}!`);
+  const nameUser = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${nameUser}! \n`);
+  for (let i = 0; i <= numberAttempt; i += 1) {
+    if (i === numberAttempt) {
+      console.log(`Congratulations, ${nameUser}!`);
       return;
     }
-    const getNumArrayRandom = Math.floor(Math.random() * (100 - 1)) + 1;
-    readlineSync.question(`Question: ${getNumArrayRandom}`);
-    const getAnswer = readlineSync.question('Answer: ');
-    if (isEven(getNumArrayRandom) === getAnswer) {
+    const randomNumber = Math.floor(Math.random() * (100 - 1)) + 1;
+    readlineSync.question(`Question: ${randomNumber}`);
+    const answer = readlineSync.question('Answer: ');
+    if (isEven(randomNumber) === answer) {
       console.log('Correct!');
-    } else { console.log(`'${getAnswer}' is wrong answer ;(. Correct answer was '${isEven(getNumArrayRandom)}'. Let's try again, ${getName}!`); break; }
+    } else { console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven(randomNumber)}'. Let's try again, ${nameUser}!`); break; }
   }
 };
 
