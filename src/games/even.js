ed58f755ea6774f9438c4bb7d4e0fa0d283de44a
Aleksei Zhighitov <allbeehappy@gmail.com>
2017-10-15 16:57:46 +0800

    Add correctAnswer

--- src/games/even.js
+++ src/games/even.js
@@ -4,11 +4,18 @@ import randomNumber from '../utils';
 
 const descriptionGame = 'Answer "yes" if number even otherwise answer "no".';
 
-const isEven = num => (num % 2 === 0 ? 'yes' : 'no');
+const isEven = num => (num % 2 === 0);
+
+const correctAnswer = (num) => {
+  if (isEven(num) === true) {
+    return 'yes';
+  }
+  return 'no';
+};
 
 const task = () => {
   const question = randomNumber(100);
-  const answer = isEven(question);
+  const answer = correctAnswer(question);
   const taskJob = cons(question, answer);
   return taskJob;
 };
