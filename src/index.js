635042f34f7560a8f43d5abcce9ae7272fc18a75
Aleksei Zhighitov <allbeehappy@gmail.com>
2017-10-15 14:18:52 +0800

    Change code to recoursion

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
-import { cdr, car } from 'hexlet-pairs';
+import { car, cdr } from 'hexlet-pairs';
 
 const numberAttempt = 3;
 
@@ -10,17 +10,27 @@ const brainGames = (rules, task) => {
   const nameUser = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${nameUser}!`);
   console.log('\n');
-  for (let i = 1; i <= numberAttempt; i += 1) {
+
+  function questionsCount(attemptCount) {
+    if (attemptCount === numberAttempt) {
+      return console.log(`Congratulations, ${nameUser}!`);
+    }
+
     const taskValue = task();
     const question = car(taskValue);
     const answer = cdr(taskValue);
     console.log(readlineSync.question(`Question: ${question}`));
     const answerUser = readlineSync.question('Answer: ');
+
     if (answer === answerUser) {
       console.log('Correct!');
-    } else { console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${answer}'. Let's try again, ${nameUser}!`); return; }
+      questionsCount(attemptCount + 1);
+    } else {
+      return console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${answer}'. Let's try again, ${nameUser}!`);
+    }
+    return questionsCount;
   }
-  console.log(`Congratulations, ${nameUser}!`);
+  return questionsCount(0);
 };
 
 export default brainGames;
