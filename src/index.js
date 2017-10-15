23d672756468604ec0c621db6742f4e89ceddecc
Aleksei Zhighitov <allbeehappy@gmail.com>
2017-10-15 20:48:01 +0800

    Change condition function questionsCount to boolean out

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import { car, cdr } from 'hexlet-pairs';
 
 const numberAttempt = 3;
 
+const questionsCount = (task, attemptCount) => {
+  if (attemptCount === numberAttempt) {
+    return true;
+  }
+
+  const taskValue = task();
+  const question = car(taskValue);
+  const answer = cdr(taskValue);
+  console.log(readlineSync.question(`Question: ${question}`));
+  const answerUser = readlineSync.question('Answer: ');
+
+  if (answer === answerUser) {
+    console.log('Correct!');
+    questionsCount(task, attemptCount + 1);
+  } else { console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${answer}'.`); return false; }
+
+  return questionsCount;
+};
+
 const brainGames = (descriptionGame, task) => {
   console.log('Welcome to the Brain Games!');
   console.log(`${descriptionGame}`);
@@ -11,26 +30,12 @@ const brainGames = (descriptionGame, task) => {
   console.log(`Hello, ${nameUser}!`);
   console.log('');
 
-  const questionsCount = (attemptCount) => {
-    if (attemptCount === numberAttempt) {
-      return console.log(`Congratulations, ${nameUser}!`);
-    }
-
-    const taskValue = task();
-    const question = car(taskValue);
-    const answer = cdr(taskValue);
-    console.log(readlineSync.question(`Question: ${question}`));
-    const answerUser = readlineSync.question('Answer: ');
-
-    if (answer === answerUser) {
-      console.log('Correct!');
-      questionsCount(attemptCount + 1);
-    } else {
-      return console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${answer}'. Let's try again, ${nameUser}!`);
-    }
-    return questionsCount;
-  };
-  return questionsCount(0);
+  const isCorrect = questionsCount(task, 0);
+  if (isCorrect) {
+    console.log(`Congratulations, ${nameUser}!`);
+  } else {
+    console.log(`Let's try again, ${nameUser}!`);
+  }
 };
 
 export default brainGames;
